f713c6b150920fa0a9a60cc23fcf5e94ee0ce6cd
harsh2003 <harsh_2003@hotmail.com>
2024-04-03 18:36:01 -0400

    update banker.c

--- Banker.c
+++ Banker.c
@@ -1,6 +1,21 @@
-#include "Banker.h"
 #include <stdio.h>
 #include <stdbool.h>
+
+#define NUM_RESOURCES 3
+#define NUM_CUSTOMERS 5
+
+extern int available[NUM_RESOURCES];
+extern int maximum[NUM_CUSTOMERS][NUM_RESOURCES];
+extern int allocated[NUM_CUSTOMERS][NUM_RESOURCES];
+extern int need[NUM_CUSTOMERS][NUM_RESOURCES];
+
+void init_banker(int available[], int max[][NUM_RESOURCES]);
+void cleanup_banker();
+void calculate_need();
+bool is_safe(int customer_id, int request[]);
+void request_resources(int customer_id, int request[]);
+void release_resources(int customer_id);
+void print_state();
 
 int available[NUM_RESOURCES] = {8, 5, 7};
 int maximum[NUM_CUSTOMERS][NUM_RESOURCES] = {
